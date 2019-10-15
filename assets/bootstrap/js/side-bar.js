6786dda7fb5728a94532011c6769bad11a30985d
andrei <webstar1987923@hotmail.com>
2019-10-16 01:28:37 +0800

    init

--- assets/bootstrap/js/side-bar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#content").width(eval($("#header").width()-9) + "px");
-    $("#content").width(eval($("#header").width()-9) + "px");
-});
-$(window).resize(function(){
-    $("#content").width(eval($("#header").width()-9) + "px");
-});
